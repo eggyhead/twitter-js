3179334ade0a6f0f1afeea56432b3185b8814efa
Hannah Weber <hkwweber@gmail.com>
2017-11-06 18:31:14 -0500

    continued working on fake twitter server

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ app.listen(3000, function() {
     console.log('your server is running on localhost:3000')
 })
 
+app.use('/', function(req,res,next) {
+  console.log('path: ',req.path);
+  console.log('method: ', req.method);
+  next();
+})
+
+app.get('/news', function(req, res, next) {
+    res.send('<h1>Welcome to fake twitter news!!!</h1>')
+})
+
 app.get('/', function(req, res, next) {
     res.send('<h1>Welcome to fake twitter</h1>')
 })
+
+
